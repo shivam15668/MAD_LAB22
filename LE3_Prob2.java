69e456c968066c292f845138ef2a7dba4dc2f4bc
Shivam Kumar <91646100+shivam15668@users.noreply.github.com>
2022-06-04 13:07:35 +0530

    Create LE3_Prob2.java

---
+++ LE3_Prob2.java
@@ -0,0 +1,31 @@
+abstract class AbsTest implements Testable{
+    public void display()
+    {
+        System.out.println("Hey");
+    }
+}
+
+/******************************************************************************
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+	//	Test t=new Test();
+	//	t.display();
+	}
+}
+
+class Test implements Testable{
+    public void display()
+    {
+        System.out.println("Hey");
+    }
+}
+
+interface Testable{
+    void display();
+}
